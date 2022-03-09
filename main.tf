63c945ad7005e84b33aa1b4025f3c4b8b68242bc
Vladyslav <vponojko@gmail.com>
2022-03-09 13:33:55 +0200

    first level

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region                      = "us-east-2"
+  access_key                  = "fake"
+  secret_key                  = "fake"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    dynamodb = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
+    kinesis  = "http://localhost:4566"
+  }
+}
+
+resource "aws_dynamodb_table" "default" {
+  name           = "default"
+  read_capacity  = "20"
+  write_capacity = "20"
+  hash_key       = "ID"
+
+  attribute {
+    name = "ID"
+    type = "S"
+  }
+}
